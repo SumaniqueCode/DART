4787f21a5e3437b273e19be1a5e5c2a8716aad6a
Suman Regmi <limitlessplaying@gmail.com>
2025-05-04 20:54:49 +0545

    Introduction to dart and its variables and data types

---
+++ 01_basic_dart.dart
@@ -0,0 +1,37 @@
+// Created by Suman Regmi
+// Date: 2025-04-05
+
+main(){ //main class is compulsory
+    print("Hello World!"); //printing statement
+
+    // Variables
+    // Data Types
+    // int - whole number
+    // double - decimal number
+    // bool - boolean value
+    // String - sequence of characters
+    // List - list of elements
+    // Map - key value pair
+    // Set - unordered collection of unique elements
+    // Null - absence of value
+    // Variables can be declared using var, final, const, late, required, required, required
+
+    //var is used to declare any type of variable
+    var name = "Suman";
+    var age = 25;
+    var height = 5.8; //height in feet
+    var isDeveloper = true;
+    print(name);
+    print(age);
+    print(height);
+    print(isDeveloper);
+
+    //specific data types
+    int number = 10;
+    double decimal = 10.5;
+    bool isTrue = true;
+    String name1 = "Suman";
+    print(number);
+    print(decimal);
+    print(isTrue);
+}
