4a388c18663ebe003c6fe09c7772c7b60dcb1c67
Suman Regmi <limitlessplaying@gmail.com>
2025-05-09 12:45:43 +0545

    Conditions in Dart

---
+++ 04_conditions.dart
@@ -0,0 +1,21 @@
+// Created by Suman Regmi
+// Date: 2025/04/06
+
+import "dart:io"; //import statement
+
+main(){
+    print("Enter any number to check its nature.");
+    int num = int.parse(stdin.readLineSync()!); // readind data from use and parsing it to int
+    if(num==0){ //if statement initialization
+    print("$num is zero.");
+    }else{
+        if(num%2 !=0){  //instead of using nested if, else if(){} can also be used
+            print("$num is odd.");
+        }else{
+            print("$num is even");
+        }
+//short conditional expression
+        num>0?print("$num is positive number.") : print('$num is negative number'); 
+    }
+    
+}
