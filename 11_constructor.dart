0f028926d8320de4b62b0a15f1672bc717bfb432
Suman Regmi <limitlessplaying@gmail.com>
2025-05-11 11:14:09 +0545

    Constructor and its types in dart

---
+++ 11_constructor.dart
@@ -0,0 +1,62 @@
+// Created by Suman Regmi
+// Date: 2025/04/11
+
+// Created by Suman Regmi
+// Date: 2025/04/06
+
+class Student {
+  String name = '';
+  int age = 0;
+  String grade = '';
+
+  // Default Constructor
+  Student() {
+    print('Default Constructor Called');
+  }
+
+  // Parameterized Constructor
+  Student.withDetails(String n, int a, String g) {
+    name = n;
+    age = a;
+    grade = g;
+  }
+
+  // Named Constructor
+  Student.fromTopper() {
+    name = 'Topper';
+    age = 16;
+    grade = 'A+';
+  }
+
+  // Constructor with initialization list
+  Student.initList() : name = 'Initialized Name', age = 20, grade = 'B' {
+    print('Initialization List Constructor Called');
+  }
+
+  void showInfo() {
+    print('Name: $name, Age: $age, Grade: $grade');
+  }
+}
+
+void main() {
+  print('\n===== Dart Constructor Demo =====\n');
+
+  // Using default constructor
+  Student s1 = Student();
+  s1.name = 'John';
+  s1.age = 15;
+  s1.grade = 'A';
+  s1.showInfo();
+
+  // Using parameterized constructor
+  Student s2 = Student.withDetails('Alice', 17, 'B+');
+  s2.showInfo();
+
+  // Using named constructor
+  Student s3 = Student.fromTopper();
+  s3.showInfo();
+
+  // Using initialization list constructor
+  Student s4 = Student.initList();
+  s4.showInfo();
+}
